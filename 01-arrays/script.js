e3b54efe1c5d2e4f508c4c7c56ce1803bb401542
Ricardo596 <ricardodiotti8@gmail.com>
2025-02-12 21:31:12 +0100

    ultimo es in forse

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -16,23 +16,39 @@ console.log(fourthTeacher["4"]);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
+
+
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
-console.log(lastTeacher)
-console.log(teachers)
+console.log(lastTeacher);
+console.log(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers.shift();
+console.log(firstTeacher);
+console.log(teachers);
 
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa");
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
-
+teachers.unshift("Sarah");
+console.log(teachers);
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers;
+lewisIndex.indexOf(`Lewis`)
+console.log(lewisIndex);
+console.log(teachers);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers;
+teachers = [];
+if(teachers.length === 0) {
+  console.log("array vuoto")
+} else {
+  console.log("array pieno")
+}
